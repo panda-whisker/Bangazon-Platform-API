e5e17a65aa00ff65941f32feba53fc7c66165ebe
dek-haji <deeeqhaji@gmail.com>
2019-08-05 11:44:33 -0500

    deleted compiler delete error

--- BangazonAPI/Controllers/CustomersController.cs
+++ BangazonAPI/Controllers/CustomersController.cs
@@ -163,11 +163,6 @@ namespace BangazonAPI.Controllers
             }
         }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(int id)
-        {
-        }
 
         private bool CustomerExists(int id)
         {
