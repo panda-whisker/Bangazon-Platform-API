671b84eb050ae5b12e79843e02900f9666258c5d
Burak Koc <burakkayakoc@gmail.com>
2019-08-06 11:24:43 -0500

    TrainingProgram Test created and works

---
+++ TestBangazonAPI/TestTrainingPrograms.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Net;
+using Newtonsoft.Json;
+using Xunit;
+using BangazonAPI.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace TestBangazonAPI
+{
+    public class TestTrainingPrograms
+    {
+        [Fact]
+        public async Task Test_Get_All_TrainingPrograms()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+
+
+                /*
+                    ACT
+                */
+                var response = await client.GetAsync("/api/trainingPrograms");
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var trainingPrograms = JsonConvert.DeserializeObject<List<TrainingProgram>>(responseBody);
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(trainingPrograms.Count > 0);
+            }
+        }
+    }
+}
