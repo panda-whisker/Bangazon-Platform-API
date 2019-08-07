4f98d7d42f30a8a965c4a01d574ae5caf71b9623
Jimmy <jimmymetts@bellsouth.net>
2019-08-07 14:04:11 -0500

    add product type get and test

---
+++ TestBangazonAPI/TestProductType.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using Xunit;
+using BangazonAPI.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestBangazonAPI
+{
+    public class TestProductTypes
+    {
+        [Fact]
+        public async Task Test_Get_All_ProductTypes()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+
+
+                /*
+                    ACT
+                */
+                var response = await client.GetAsync("/api/productType");
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var productTypes = JsonConvert.DeserializeObject<List<ProductType>>(responseBody);
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(productTypes.Count > 0);
+            }
+        }
+
+
+
+
+
+    }
+}
