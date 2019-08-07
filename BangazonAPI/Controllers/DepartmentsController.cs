08177753b698d8f4a851e0e858795f99df5b70db
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-08-07 15:46:27 -0500

    all tests passing -- working on applicable search queries

---
+++ BangazonAPI/Controllers/DepartmentsController.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        
+        public DepartmentsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        private SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET api/departments
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            string SqlCommandText = @"SELECT d.Id, d.Name, d.Budget
+                                      FROM Department d";
+
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = SqlCommandText;
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+                    List<Department> departments = new List<Department>();
+                    while (reader.Read())
+                    {
+                        Department department = new Department
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Budget = reader.GetInt32(reader.GetOrdinal("Budget"))
+                        };
+
+                        departments.Add(department);
+                    }
+
+                    reader.Close();
+
+                    return Ok(departments);
+                }
+            }
+        }
+
+        [HttpGet("{id}", Name = "GetDepartment")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, Name, Budget
+                                        FROM Department WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Department department = null;
+                    if (reader.Read())
+                    {
+                        department = new Department
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Budget = reader.GetInt32(reader.GetOrdinal("Budget"))
+                        };
+                    }
+
+                    reader.Close();
+                    return Ok(department);
+                }
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Department department)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    //More string interpolation
+                    cmd.CommandText = @"
+                         INSERT INTO Department (Name, Budget)
+                         OUTPUT INSERTED.Id
+                         VALUES (@name, @budget)";
+                    cmd.Parameters.Add(new SqlParameter("@name", department.Name));
+                    cmd.Parameters.Add(new SqlParameter("@budget", department.Budget));
+
+                    department.Id = (int)await cmd.ExecuteScalarAsync();
+
+                    return CreatedAtRoute("GetDepartment", new { id = department.Id }, department);
+                }
+            }
+        }
+
+        // PUT api/computers/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Department department)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"
+                             UPDATE Department
+                             SET Name = @name,
+                                 Budget = @budget
+                             WHERE Id = @id
+                         ";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+                        cmd.Parameters.Add(new SqlParameter("@name", department.Name));
+                        cmd.Parameters.Add(new SqlParameter("@budget", department.Budget));
+
+                        int rowsAffected = await cmd.ExecuteNonQueryAsync();
+
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private bool DepartmentExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    //More string interpolation
+                    cmd.CommandText = "SELECT Id FROM Department WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    return reader.Read();
+                }
+            }
+        }
+    }
+}
---
+++ TestBangazonAPI/TestDepartments.cs
@@ -0,0 +1,150 @@
+﻿using BangazonAPI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestBangazonAPI
+{
+    public class TestDepartments
+    {
+        [Fact]
+        public async Task Test_Get_All_Departments()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+
+
+                /*
+                    ACT
+                */
+                var response = await client.GetAsync("/api/departments");
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var departments = JsonConvert.DeserializeObject<List<Department>>(responseBody);
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(departments.Count > 0);
+            }
+        }
+
+        [Fact]
+        public async Task Test_Get_Single_Department()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+
+                /*
+                    ACT
+                */
+                var response = await client.GetAsync("/api/departments/1");
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var department = JsonConvert.DeserializeObject<Department>(responseBody);
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.Equal(department.Name, department.Name);
+                Assert.Equal(department.Budget, department.Budget);
+                Assert.NotNull(department);
+            }
+        }
+
+        [Fact]
+        public async Task Test_Create_Department()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+                Department HR = new Department
+                {
+                    Name = "Human Resources",
+                    Budget = 7000
+                };
+                var HRAsJSON = JsonConvert.SerializeObject(HR);
+
+                /*
+                    ACT
+                */
+                var response = await client.PostAsync(
+                    "/api/departments",
+                    new StringContent(HRAsJSON, Encoding.UTF8, "application/json")
+                );
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var NewDepartment = JsonConvert.DeserializeObject<Department>(responseBody);
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+                Assert.Equal(HR.Name, NewDepartment.Name);
+                Assert.Equal(HR.Budget, NewDepartment.Budget);
+
+                /*
+                    ACT
+                */
+            }
+        }
+
+        [Fact]
+        public async Task Test_Modify_Department()
+        {
+            // New eating habit value to change to and test
+            string NewDept = "Management";
+
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    PUT section
+                 */
+                Department ModifiedDepartment = new Department
+                {
+                    Name = NewDept,
+                    Budget = 30000
+                };
+                var ModifiedDeptAsJSON = JsonConvert.SerializeObject(ModifiedDepartment);
+
+                var response = await client.PutAsync(
+                    "/api/departments/3",
+                    new StringContent(ModifiedDeptAsJSON, Encoding.UTF8, "application/json")
+                );
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+
+                /*
+                    GET section
+                 */
+                var GetDept = await client.GetAsync("/api/departments/3");
+                GetDept.EnsureSuccessStatusCode();
+
+                string GetDeptBody = await GetDept.Content.ReadAsStringAsync();
+                Department NewDepart = JsonConvert.DeserializeObject<Department>(GetDeptBody);
+
+                Assert.Equal(HttpStatusCode.OK, GetDept.StatusCode);
+                Assert.Equal(NewDept, NewDepart.Name);
+            }
+        }
+    }
+}
